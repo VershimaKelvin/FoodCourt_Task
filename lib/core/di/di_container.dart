094d55684282e5c291d40713c93e3ad486791ee2
VershimaKelvin <kelvinityavyar@gmail.com>
2023-02-21 10:21:40 +0100

    fix: initialized my service locator and fixed the issue that was persisting

--- lib/core/di/di_container.dart
+++ lib/core/di/di_container.dart
@@ -2,6 +2,8 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:task/core/di/di_container.config.dart';
 
+
 GetIt di = GetIt.instance;
 @injectableInit
-Future<void> configureDependencies() => di.init();
+
+Future<void> configureDependencies() => $initGetIt(di);
--- lib/core/network/api_requester.dart
+++ lib/core/network/api_requester.dart
@@ -1,19 +1,19 @@
-import 'package:dio/dio.dart';
-import 'package:injectable/injectable.dart';
-import 'package:task/core/constant/network_client.dart';
-
-
-@lazySingleton
-class ApiRequester {
-  ApiRequester({
-    required this.dio,
-  });
-  final Dio dio;
-
-  Future<Response> getRequest({required String? queryParam}) async {
-    final response = dio.get(
-      BASE_URL + queryParam!,
-    );
-    return response;
-  }
-}
+// import 'package:dio/dio.dart';
+// import 'package:injectable/injectable.dart';
+// import 'package:task/core/constant/network_client.dart';
+//
+//
+// @lazySingleton
+// class ApiRequester {
+//   ApiRequester({
+//     required this.dio,
+//   });
+//   final Dio dio;
+//
+//   Future<Response> getRequest({required String? queryParam}) async {
+//     final response = dio.get(
+//       BASE_URL + queryParam!,
+//     );
+//     return response;
+//   }
+// }
